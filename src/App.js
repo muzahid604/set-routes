b44d4f479f501354772f67d01cc9881128527b77
muzahid604 <604muzahid@gmail.com>
2022-04-01 17:36:14 +0600

    link path added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Friends from './componets/Friends/Friends';
+import Heder from './componets/Header/Heder';
 import Home from './componets/Home/Home'
 
 function App() {
   return (
     <div className="App">
+      <Heder></Heder>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/friends' element={<Friends />}></Route>
---
+++ src/componets/Header/Heder.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Heder = () => {
+    return (
+        <div>
+            <h1>Hello Bangladesh</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/friends">Friends</Link>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Heder;
